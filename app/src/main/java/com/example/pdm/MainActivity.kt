78173850a0c564e117459afd3bd2674cdf5fd397
Kaique Miranda <exhardkaique@gmail.com>
2022-04-12 18:20:43 -0300

    Página inicial e área do aluno

--- app/src/main/java/com/example/pdm/MainActivity.kt
+++ app/src/main/java/com/example/pdm/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.pdm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnConectar: Button = findViewById(R.id.btnConectar)
+        btnConectar.setOnClickListener {
+            val edtRA: EditText = findViewById(R.id.edtRA)
+            val edtSenha: EditText = findViewById(R.id.edtSenha)
+            val sRA = edtRA.text
+            val sSenha = edtSenha.text
+            Toast.makeText(applicationContext, "Login realizado com sucesso!", Toast.LENGTH_LONG).show()
+            // criar intent
+            val telaAreaDoAluno = Intent(this, AreaDoAluno::class.java)
+
+            // Passando parâmetros para a segunda tela
+            val parametros = Bundle()
+            parametros.putString("ra", "$sRA")
+            telaAreaDoAluno.putExtras(parametros)
+
+            // fazer a chamada
+            startActivity(telaAreaDoAluno)
+        }
     }
 }
